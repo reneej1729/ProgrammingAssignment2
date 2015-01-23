8e4bb09aa9f5af45a8310226fdde39b6dcf95cb5
reneej1729 <reneej1729@gmail.com>
2015-01-23 13:02:03 -0800

    Filled file with my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions that cache the inverse of matrix, if it has already
+## been calculated. Otherwise, the inverse is calculated and stored for later reference.
 
-## Write a short comment describing this function
+# makeCacheMatrix takes in an invertible matrix of numbers, and
+# returns a matrix that contains 4 functions:
+# element [1,1]: the set function
+# element [1,2]: the get function
+# element [2,1]: the setsolve function
+# element [2,2]: the getsolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- x[[2,2]]()                       # uses getsolve to pull the current cached value
+     if(!is.null(m)){                      # if a cached value exists, returns the value
+          message("getting cached data")
+          return(m)
+     }
+     
+     data <- x[[2,1]]()                    # uses get to pull the data
+     m <- solve(data)                      # finds the inverse, if not already done
+     x[[1,2]](m)                           # calls setsolve to store the inverse value
+     m 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve takes in a matrix made by makeMatrix and returns the inverse.
+# It first checks to see if the inverse has previously been calculated and will
+# return the cached answer, if it exists.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m <- x[[2,2]]()                       # uses getsolve to pull the current cached value
+     if(!is.null(m)){                      # if a cached value exists, returns the value
+          message("getting cached data")
+          return(m)
+     }
+     
+     data <- x[[2,1]]()                    # uses get to pull the data
+     m <- solve(data)                      # finds the inverse
+     x[[1,2]](m)                           # calls setsolve to store the inverse value
+     m 
 }
